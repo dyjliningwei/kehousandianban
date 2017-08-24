d8a4406dc4267d697b0100b119cd217c663b7dbb
li <liningwei@liningweideMacBook-Pro.local>
2017-08-24 10:07:29 +0800

    修改样式

--- js/shareSchool.js
+++ js/shareSchool.js
@@ -3,21 +3,3 @@ $('.abstractDown').on('click', function () {
     $('.courseP').toggleClass('courseP-active');
     $('.abstractDown .iconfont').toggleClass('down-active');
 })
-
-
-new function () {
-    var _self = this;
-    _self.width = 750; //设置默认最大宽度
-    _self.fontSize = 20; //默认字体大小
-    _self.widthProportion = function () {
-        var p = (document.body && document.body.clientWidth )/ _self.width;
-        return p > 1 ? 1 : p < 0.5 ? 0.5 : p;
-    };
-    _self.changePage = function ()   {
-        document.getElementsByTagName("html")[0].setAttribute("style", "font-size:" + _self.widthProportion() * _self.fontSize + "px");
-    }
-    _self.changePage();
-    window.addEventListener('resize', function () {
-        _self.changePage();
-    }, false);
-};
